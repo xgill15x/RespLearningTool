0a991449fe008dc46c559410976415225d706ab7
xgill15x <jason_gill15@hotmail.com>
2024-11-20 02:08:21 -0800

    add images

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React, { useState, useRef } from "react";
 
 const App = () => {
   const sounds = [
-    "/sounds/Bronchial_1_1.mp3",
-    "/sounds/Broncho Vesicular_1_1.mp3",
-    "/sounds/Cavernous_1_1.mp3",
-    "/sounds/Coarse Crackles_1_1.mp3",
-    "/sounds/Egophony_1_1.mp3",
-    "/sounds/Medium To Fine Crackles_1_1.mp3",
-    "/sounds/Monophonic Wheeze_1_1.mp3",
-    "/sounds/Pectoriloquy_1_1.mp3",
-    "/sounds/Plural Friction Rub_1_1.mp3",
-    "/sounds/Pulmonary Edema_1_1.mp3",
-    "/sounds/Rhonci Crackles_1_1.mp3",
-    "/sounds/Stridor_1_1.mp3",
-    "/sounds/Tracheal_1_1.mp3",
-    "/sounds/Vesicular_1_1.mp3",
-    "/sounds/Wheeze_1_1.mp3",
+    `${process.env.PUBLIC_URL}/sounds/Bronchial_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Broncho Vesicular_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Cavernous_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Coarse Crackles_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Egophony_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Medium To Fine Crackles_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Monophonic Wheeze_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Pectoriloquy_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Plural Friction Rub_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Pulmonary Edema_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Rhonci Crackles_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Stridor_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Tracheal_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Vesicular_1_1.mp3`,
+    `${process.env.PUBLIC_URL}/sounds/Wheeze_1_1.mp3`,
+  ];
+
+  const correctImages = [
+    `${process.env.PUBLIC_URL}/images/puppy-thumbs-up.gif`,
+    `${process.env.PUBLIC_URL}/images/jon-jones.gif`,
+    `${process.env.PUBLIC_URL}/images/good-job.gif`,
+  ];
+
+  const incorrectImages = [
+    `${process.env.PUBLIC_URL}/images/disappointed.jpeg`,
+    `${process.env.PUBLIC_URL}/images/smh-kanye-west.gif`,
+    `${process.env.PUBLIC_URL}/images/squidward-stressed.gif`,
   ];
 
   const [currentSound, setCurrentSound] = useState("");
   const [userGuess, setUserGuess] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [feedbackImage, setFeedbackImage] = useState("");
 
   const audioRef = useRef(null);
 
@@ -37,11 +50,11 @@ const App = () => {
 
     const audio = new Audio(randomSound);
     audioRef.current = audio;
-    console.log(audio);
     audio.play();
 
     setUserGuess("");
     setFeedback("");
+    setFeedbackImage("");
   };
 
   const repeatSound = () => {
@@ -68,14 +81,20 @@ const App = () => {
 
     if (normalize(userGuess) === normalize(correctAnswer)) {
       setFeedback("Correct!");
+      const randomCorrectImage =
+        correctImages[Math.floor(Math.random() * correctImages.length)];
+      setFeedbackImage(randomCorrectImage);
     } else {
       setFeedback(`Incorrect! The correct answer is: ${correctAnswer}`);
+      const randomIncorrectImage =
+        incorrectImages[Math.floor(Math.random() * incorrectImages.length)];
+      setFeedbackImage(randomIncorrectImage);
     }
   };
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Random Lung Sounds Player</h1>
+      <h1>Random Bad Lung Sound Player (Difficulty: Expert)</h1>
       <button
         onClick={playRandomSound}
         style={{
@@ -142,15 +161,28 @@ const App = () => {
       )}
 
       {feedback && (
-        <div
-          style={{
-            marginTop: "20px",
-            color: feedback.startsWith("Correct") ? "green" : "red",
-            fontSize: "18px",
-            fontWeight: "bold",
-          }}
-        >
-          {feedback}
+        <div style={{ marginTop: "20px" }}>
+          <p
+            style={{
+              color: feedback.startsWith("Correct") ? "green" : "red",
+              fontSize: "18px",
+              fontWeight: "bold",
+            }}
+          >
+            {feedback}
+          </p>
+          {feedbackImage && (
+            <img
+              src={feedbackImage}
+              alt={feedback.startsWith("Correct") ? "Correct" : "Incorrect"}
+              style={{
+                maxWidth: "90%",
+                maxHeight: "300px",
+                objectFit: "contain",
+                marginTop: "10px",
+              }}
+            />
+          )}
         </div>
       )}
     </div>
